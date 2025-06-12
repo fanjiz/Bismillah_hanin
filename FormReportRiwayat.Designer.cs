a01e59ee6470e3ae44396f5c357efff973cb8014
fanjiz <irpanaziz232@gmail.com>
2025-06-13 02:45:59 +0700

    crystal report works

--- FormReportRiwayat.Designer.cs
+++ FormReportRiwayat.Designer.cs
@@ -28,25 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CrystalReport11 = new Bismillah_Hanin.CrystalReport1();
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReport12 = new Bismillah_Hanin.CrystalReport1();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.ActiveViewIndex = 0;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(672, 450);
+            this.crystalReportViewer1.ReportSource = this.CrystalReport12;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // FormReportRiwayat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 450);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "FormReportRiwayat";
             this.Text = "FormReportRiwayat";
@@ -55,7 +58,8 @@
         }
 
         #endregion
-
+        private CrystalReport1 CrystalReport11;
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalReport1 CrystalReport12;
     }
 }
